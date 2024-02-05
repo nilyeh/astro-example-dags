1f174166723e734f2049f58cdcac7de7a88b732b
nilyeh <128098727+nilyeh@users.noreply.github.com>
2024-02-05 10:03:28 -0600

    Update 03_load_project.py
    Para prueba resolver error: 
    
     Broken DAG: [/usr/local/airflow/dags/03_load_project.py] Traceback (most recent call last):
      File "<frozen importlib._bootstrap>", line 241, in _call_with_frames_removed
      File "/usr/local/airflow/dags/03_load_project.py", line 28, in <module>
        dag=dag,
            ^^^
    NameError: name 'dag' is not defined

--- dags/03_load_project.py
+++ dags/03_load_project.py
@@ -28,7 +28,11 @@ install_db_dtypes = BashOperator(
     dag=dag,
 )
 
-
+dag = DAG(
+    'install_db_dtypes',
+    default_args=default_args,
+    schedule_interval='@once',
+    
 def get_connect_mongo():
 
     CONNECTION_STRING ="mongodb+srv://atlas:T6.HYX68T8Wr6nT@cluster0.enioytp.mongodb.net/?retryWrites=true&w=majority"
